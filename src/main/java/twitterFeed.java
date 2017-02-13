a4d810da9c4d8abf9fe47acdfc5ab3a47e382f24
yogesh verma <yogesh.verma@target.com>
2017-02-13 14:46:23 +0530

    change file path to general file path

--- src/main/java/twitterFeed.java
+++ src/main/java/twitterFeed.java
@@ -2,18 +2,26 @@
  * Created by z00295r on 2/10/17.
  */
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class twitterFeed {
 
     public static void main(String[] args) throws TwitterException {
-
+        int count = 0;
+        int columnCount = 0;
+        int rowCount = 0;
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Java Books");
         ConfigurationBuilder cb = new ConfigurationBuilder();
-
-
 
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("")
@@ -21,26 +29,44 @@ public class twitterFeed {
                 .setOAuthAccessToken("")
                 .setOAuthAccessTokenSecret("");
 
+
+
         TwitterFactory tf = new TwitterFactory(cb.build());
-        twitter4j.Twitter twitter  =  tf.getInstance();
+//        Twitter twitterHash = tf.getInstance();
+        Twitter twitter = tf.getInstance();
 
-        //get username, status
-
-       List<Status> status = twitter.getHomeTimeline();
-        System.out.println(status.size());
-        for(Status st : status){
-            System.out.println(st.getUser().getName()+ "-------" + st.getText());
-        }
-
-        twitter4j.Twitter twitterTimeline =  TwitterFactory.getSingleton();
         Query query = new Query("#target");
-        QueryResult result = twitterTimeline.search(query);
-        System.out.println(result.getCount());
-
-        for (Status statuses : result.getTweets()) {
-            System.out.println("@" + statuses.getUser().getScreenName() + " : " + statuses.getText() + " : " + statuses.getGeoLocation());
+        QueryResult result;
+        Paging pg = new Paging();
+        pg.setCount(200);
+        String accountName = "@unity3d";
+        List<Status> status = twitter.getUserTimeline(accountName, pg);
+        System.out.println(status.size());
+        for (Status st : status) {
+            System.out.println(st.getUser().getName() + "-------" + st.getText());
         }
+        for (Status tweet : status) {
+            count++;
+//                    for (Object aBook : feedData) {
+            Row row = sheet.createRow(rowCount++);
 
+//                    for(Object aBook : tweet){
+            Cell cell = row.createCell(columnCount++);
+            cell.setCellValue(tweet.getUser().getScreenName());
+            Cell cell1 = row.createCell(columnCount);
+            cell1.setCellValue(tweet.getText());
+            columnCount = 0;
+            System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+        }
+        try{
+            FileOutputStream outputStream = new FileOutputStream(new File("FilePath/JavaBooks.csv"));
+            workbook.write(outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
+
+
+
